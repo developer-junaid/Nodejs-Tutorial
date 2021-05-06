19d7153576b933a98e8762d9aa23e61bfc5dd80e
developer-junaid <qjunaid623@gmail.com>
2021-05-06 15:42:27 +0500

    Completed,  Some topics
    * Global Object
    * Function Expressions
    * Modules and require()
    * Module patterns
    * Node event emitter

---
+++ modules/count.js
@@ -0,0 +1,5 @@
+const counter = (arr) => {
+  return `There are ${arr.length} elements in this array`;
+};
+
+module.exports = counter;
---
+++ modules/stuff.js
@@ -0,0 +1,15 @@
+const counter = (arr) => {
+  return `There are ${arr.length} elements in this array`;
+};
+
+const adder = (a, b) => {
+  return `The sum of the numbers is ${a + b}`;
+};
+
+const pi = 3.142;
+
+module.exports = {
+  counter,
+  adder,
+  pi,
+};
