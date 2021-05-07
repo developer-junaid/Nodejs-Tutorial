c3a66a66985e0c555264d4c0a6bccddecbe7a9ca
developer-junaid <qjunaid623@gmail.com>
2021-05-08 04:43:27 +0500

    Patterns

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Please follow the branches for further topics
 // 3. Modules and require
 
-let counter = require('./modules/count')
+let {counter, adder, pi} = require('./modules/count')
 
-console.log(counter(['Junaid', 'Hassan', 'Jani']));
+console.log('counter', counter(['Junaid', 'Hassan', 'Jani']));
+console.log('adder', adder(5,5));
+console.log('pi', pi);
--- modules/count.js
+++ modules/count.js
@@ -2,4 +2,23 @@ let counter = function(arr){
     return `There are ${arr.length} elements in this array`
 }
 
-module.exports = counter
+
+let adder = (a, b) =>{
+    return `Sum of the numbers is ${a+b}`
+}
+
+let pi = 3.142;
+
+
+
+// Exporting - 1
+// module.exports.counter = counter
+// module.exports.adder = adder
+// module.exports.pi = pi
+
+// Exporting - 2
+module.exports = {
+    counter,
+    adder,
+    pi
+}
