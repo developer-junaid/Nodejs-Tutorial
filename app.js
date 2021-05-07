f15d6adc0cb7ffa4a7d9827498754f10d8500f51
developer-junaid <qjunaid623@gmail.com>
2021-05-08 04:26:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Please follow the branches for further topics
+// 2. Function Expressions
+
+// Normal Function
+function sayHi(){
+    console.log('Hi');
+}
+
+sayHi()
+
+// Function expression
+let sayBye = function(){
+    console.log('Bye');
+}
+
+sayBye()
+
+// Passing function to a function
+function saySomething (fun) {
+    fun()
+}
+
+saySomething(sayBye)
