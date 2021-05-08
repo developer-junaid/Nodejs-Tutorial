1d143aa392ced3d3f1311ed90257a64339245508
developer-junaid <qjunaid623@gmail.com>
2021-05-08 15:10:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Please follow the branches for further topics
+// 7. Creating Server
+const { createServer } = require('http')
+
+// Create Server
+let server = createServer((req, resp)=>{
+    // When Request is made
+    console.log(`request was made from: ${req.url}`);
+
+    // Create Headers
+    // writeHead(status, Headers)
+    resp.writeHead(200, {'Content-Type':'text/plain'})
+
+    // Send response
+    resp.end('Hey Ninja !')
+
+})
+
+
+// Specifying port
+// listen(port, Ip) - localhost (127.0.0.1)
+server.listen(3000, '127.0.0.1')
+console.log(`server listening to port ${3000}`);
