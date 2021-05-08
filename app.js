f196361fb351083c8f8f815ac846d48c9f2d9ad9
developer-junaid <qjunaid623@gmail.com>
2021-05-08 14:47:19 +0500

    Added files

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // Please follow the branches for further topics
+// 6. Creating and Removing directories
+let {unlink, mkdirSync, rmdirSync, rmdir, mkdir, readFile, writeFile} = require('fs')
+
+// Deleting file
+// unlink('file.txt', (err)=>{
+//     if(err) console.log(err);
+// })
+
+
+// SYNCHRONOUS //
+// Creating Directory
+// mkdirSync('folder')
+
+// Removing Directory
+// rmdirSync('folder')
+
+// ASYNCHRONOUS //
+// Creating Directory
+// mkdir('data', function(){
+//     // Read File
+//     readFile('./readMe.txt', 'utf8', function(err, data){
+//         // Write file
+//         writeFile('./data/writeMe.txt', data, (err)=>{
+//             if(err) console.log('err', err);
+//         })
+//     })
+// })
+
+// Make Directory Empty
+unlink('./data/writeMe.txt', ()=>{
+    // Remove Directory
+    rmdir('data', (err)=>{
+        if (err) console.log('err');
+    })
+})
