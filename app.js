e5d104d537921ece8fdff668f77bfaa5155b37f5
developer-junaid <qjunaid623@gmail.com>
2021-05-16 14:39:18 +0500

    Routing Done
    * Send HTML Pages to routes
    * Send Json Data to Routes
    * Send 404 page

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 // Please follow the branches for further topics
 // 12. Basic Routing
 const { createServer } = require("http");
+const { createReadStream } = require("fs");
 
 // Create Server
 let server = createServer((req, resp) => {
   // When Request is made
   console.log(`request was made from: ${req.url}`);
 
-  // Create Headers
-  // writeHead(status, Headers)
-  resp.writeHead(200, { "Content-Type": "text/plain" });
+  // If Route = /home or /
+  if (req.url === "/home" || req.url == "/") {
+    // Send Home page
+    resp.writeHead(200, { "Content-Type": "text/html" });
+    createReadStream(__dirname + "/index.html").pipe(resp);
+  } else if (req.url === "/contact") {
+    // If it is contact
+    // Send contact Pag
+    resp.writeHead(200, { "Content-Type": "text/html" });
+    createReadStream(__dirname + "/contact.html").pipe(resp);
+  } else if (req.url === "/api/ninjas") {
+    // If this is api request
+    var ninjas = [
+      { name: "junaid", age: 23 },
+      { name: "hassan", age: 25 },
+    ];
 
-  // Send response
-  resp.end("Hey It's Junaid here !");
+    // Send the data
+    resp.writeHead(200, { "Content-Type": "application/json" });
+    // Send Data
+    resp.end(JSON.stringify(ninjas));
+  } else {
+    // Send Not found page
+    resp.writeHead(404, { "Content-Type": "text/html" });
+    createReadStream(__dirname + "/404.html").pipe(resp);
+  }
 });
 
 // Specifying port
