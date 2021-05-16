921d06a8ed22f4a852a25da354fba4284221f79c
developer-junaid <qjunaid623@gmail.com>
2021-05-16 15:28:38 +0500

    Creating Views (Templates), Installing EJS, Dynamic Data

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 // Please follow the branches for further topics
-// 14. Express (Route Params)
+// 15. Express (Template Engines) EJS
 const express = require("express");
 
 // Create express app
 const app = express();
 
+// Set view engine
+app.set("view engine", "ejs");
+
 // Get request
 // app.get(route, function)
 app.get("/", (req, res) => {
-  // Respond
-  res.send("this is the homepage"); // Send String
+  // Respond with html
+  res.sendFile(__dirname + "/index.html"); // Send html
 });
 
 app.get("/contact", (req, res) => {
-  // Respond
-  res.send("this is the contact page"); // Send String
+  // Respond with html
+  res.sendFile(__dirname + "/contact.html"); // Send html
 });
 
 // Dynamic route
 app.get("/profile/:id", (req, res) => {
-  // Respond
-  res.send(`You requested to see a profile with the id of ${req.params.id}`);
+  // Data
+  var data = {
+    age: 29,
+    job: "ninja",
+  };
+
+  // Respond with template
+  res.render("profile", { person: req.params.id, data });
 });
 
 // Listen to port
